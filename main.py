1f4dd651d3465b1731a83ed3f557ea8cfc339794
lloydkoh <58274238+lloydkoh@users.noreply.github.com>
2023-05-02 15:20:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,8 @@ def load_LLM(openai_api_key):
     llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
     return llm
 
+st.header("LLOYD MADE THIS APP")
+
 st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
 st.header("Email Generator")
 st.write("This app helps non-native English speakers communicate professionally via email. Submit your text, and our algorithms will analyze and format it for visual appeal with clear headings and paragraphs. Perfect for professionals, students, or anyone looking to improve their English writing skills.")
